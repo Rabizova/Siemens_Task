ea24b4f4eb98b05a828af9b48cf6f1cba3990763
MarinaRabizova <mrabizova@mail.ru>
2021-07-26 16:43:47 +0300

    Print directory list

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print("Hello, Siemens!")
+from os import listdir
+from os.path import isfile, join
+
+
+my_path = '../logs'
+
+directory_list = []
+
+for f in listdir(my_path):
+    directory_list.append(f)
+
+print(directory_list)
