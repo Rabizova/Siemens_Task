286a5fcc9f09a577fdee7d2cbd9f55e16613fe9f
MarinaRabizova <mrabizova@mail.ru>
2021-07-26 18:59:00 +0300

    add report.txt and stdout like eference_result.txt

--- src/main.py
+++ src/main.py
@@ -6,11 +6,15 @@ logs_path = '../logs'
 
 def test_logs(path):
     ft_dir = listdir(path)
-    # local_file = open(path + '/report.txt')
+    local_file = open(path + '/report.txt', 'w')
 
     if 'ft_run' not in ft_dir:
+        local_file.write('directory missing: ft_run')
+        local_file.close()
         return ['directory missing: ft_run']
     if 'ft_reference' not in ft_dir:
+        local_file.write('directory missing: ft_reference')
+        local_file.close()
         return ['directory missing: ft_reference']
 
     tests_dir_run = listdir(path + '/' + 'ft_run')
@@ -31,7 +35,7 @@ def test_logs(path):
         # asd
     error_path_run = []
     error_msg_run = 'In ft_run there are extra files not present in ft_reference:'
-    for test in tests_dir_ref:
+    for test in tests_dir_run:
         if not os.path.exists(path + '/' + 'ft_run' + '/' + test):
             error_path_run.append(path + '/' + 'ft_run' + '/' + test)
 
@@ -41,8 +45,17 @@ def test_logs(path):
         # print(error_lines)
 
     if error_lines:
+        for error_line in answer:
+            if type(error_line) == type([]):
+                for word in error_line:
+                    local_file.write(word)
+            else:
+                local_file.write(error_line + '\n')
+        local_file.close()
         return error_lines
 
+    local_file.write('OK')
+    local_file.close()
     return ['OK']
 
 
@@ -50,21 +63,19 @@ directory_list = listdir(logs_path)
 
 # print(directory_list)
 
-file = open('../reference_result.txt', 'w')
 
 for i in range(len(directory_list)):
     if os.path.isdir(logs_path + '/' + directory_list[i]):
         for directory in listdir(logs_path + '/' + directory_list[i]):
-            answer = test_logs(logs_path + '/' + directory_list[i] + '/' + directory)
-            for line in answer:
-                print(line)
-                if line == 'OK':
-                    file.write('OK: ' + directory_list[i] + '/' + directory + '/\n')
-                else:
-                    file.write('FAIL: ' + directory_list[i] + '/' + directory + '/\n')
-                    if type(line) == type([]):
-                        for a in line:
-                            file.write(a)
+            if os.path.isdir(logs_path + '/' + directory_list[i] + '/' + directory):
+                answer = test_logs(logs_path + '/' + directory_list[i] + '/' + directory)
+                for line in answer:
+                    if line == 'OK':
+                        print('OK: ' + directory_list[i] + '/' + directory + '/\n', end='')
                     else:
-                        file.write(line + '\n')
-file.close()
+                        print('FAIL: ' + directory_list[i] + '/' + directory + '/\n', end='')
+                        if type(line) == type([]):
+                            for a in line:
+                                print(a, end='')
+                        else:
+                            print(line + '\n', end='')
