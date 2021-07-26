72a5c3296fb78527e5e870a0c188881dc44b27bc
MarinaRabizova <mrabizova@mail.ru>
2021-07-26 16:55:01 +0300

    Create test_logs func

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 from os import listdir
-from os.path import isfile, join
 
 
-my_path = '../logs'
-
+logs_path = '../logs'
 directory_list = []
 
-for f in listdir(my_path):
+
+def test_logs(path):
+    print('\t' + path)
+    pass
+
+
+for f in listdir(logs_path):
     directory_list.append(f)
 
 print(directory_list)
@@ -14,5 +18,6 @@ print(directory_list)
 for i in range(len(directory_list)):
     print(directory_list[i])
 
-    for directory in listdir(my_path + '/' + directory_list[i]):
-        print('\t' + directory)
+    for directory in listdir(logs_path + '/' + directory_list[i]):
+        test_logs(logs_path + '/' + directory_list[i] + '/' + directory)
+
