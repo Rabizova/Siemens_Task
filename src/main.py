72a7e67eca2a56af4efc6166491edcd442f76905
MarinaRabizova <mrabizova@mail.ru>
2021-07-26 17:22:15 +0300

    Task 1

--- src/main.py
+++ src/main.py
@@ -2,22 +2,22 @@ from os import listdir
 
 
 logs_path = '../logs'
-directory_list = []
 
 
 def test_logs(path):
-    print('\t' + path)
-    pass
+    ft_dir = listdir(path)
+
+    if ('ft_run' not in ft_dir) or ('ft_reference' not in ft_dir):
+        print('directory missing: ' + path)
 
 
-for f in listdir(logs_path):
-    directory_list.append(f)
 
-print(directory_list)
+directory_list = listdir(logs_path)
+
+# print(directory_list)
 
 for i in range(len(directory_list)):
-    print(directory_list[i])
-
+    # print(directory_list[i])
     for directory in listdir(logs_path + '/' + directory_list[i]):
         test_logs(logs_path + '/' + directory_list[i] + '/' + directory)
 
