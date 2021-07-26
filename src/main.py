47d88dc986b46d9aa8fba21772657bb8837d9ef3
MarinaRabizova <mrabizova@mail.ru>
2021-07-26 16:49:33 +0300

    Print inner directories

--- src/main.py
+++ src/main.py
@@ -10,3 +10,9 @@ for f in listdir(my_path):
     directory_list.append(f)
 
 print(directory_list)
+
+for i in range(len(directory_list)):
+    print(directory_list[i])
+
+    for directory in listdir(my_path + '/' + directory_list[i]):
+        print('\t' + directory)
